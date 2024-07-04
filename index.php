41506ef5ca41b8a63e6a49f1c33bef82472d58ed
Raisul Islam <mdraju3248@gmail.com>
2024-07-05 00:45:28 +0600

    simple header create

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>User - Login and Register</title>
 </head>
 <body>
-    
+    <header>
+        <h2>User - Login and Register</h2>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Blog</a>
+            <a href="#">Contact</a>
+        </nav>
+        <div class="sign-in-up">
+            <button type="button">LOGIN</button>
+            <button type="button">REGISTER</button>
+        </div>
+    </header>
 </body>
 </html>
